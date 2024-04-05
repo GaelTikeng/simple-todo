ba366ef0232ebcb70c02b14e82ef3beab105dc93
GaelTikeng <Gaelinho@yahoo.fr>
2024-04-05 06:48:23 +0100

    done with the design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,49 @@
-"use client"
+"use client";
 import Image from "next/image";
-import React from "react"
-import styled from "@emotion/styled"
+import React from "react";
+import styled from "@emotion/styled";
+import dynamic from "next/dynamic";
+
+const DisplayTodo = dynamic(() => import ('@/core/component/organisms/displayTodo'), {ssr: false})
 
 export default function Home() {
   const StyledPage = styled.div`
     min-height: 100vh;
-    background-color; #f7f8fc
+    background-color: #fff;
+    width: 70%;
+    margin: 0 auto;
+  `;
+  const ButtonAndInput = styled.div`
+    display: flex;
+    gap: 1rem;
   `
+
+  const TodoWrapper = styled.div`
+
+  `
+
+  // ADD TODO
+  const addTodo = () => {
+    console.log("add todo")
+  }
+
   return (
     <StyledPage>
       <div>
         <h1>Todo list with firebase</h1>
-        <p>Deploy<span>-&gt;</span></p>
+        <ButtonAndInput>
+          <input
+            type="text"
+            placeholder="Enter todo"
+            className="input-field"
+          />
+          <button onClick={addTodo}>
+            Add todo
+          </button>
+        </ButtonAndInput>
+        <TodoWrapper>
+          <DisplayTodo/>
+        </TodoWrapper>
       </div>
     </StyledPage>
   );
